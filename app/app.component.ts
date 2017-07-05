47bd236b3563b18896cde734aae54e5371251577
James Lannon <Guest@Epicodus-6B.local>
2017-07-05 15:00:33 -0700

    add property bindings

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
        <h1>To Do List for {{month}}/{{day}}/{{year}}</h1>
        <h3>{{currentFocus}}</h3>
        <ul>
-         <li (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
-       </ul>
+         <li [class]="priorityColor(currentTask)" (click)="isDone(currentTask)" *ngFor="let currentTask of tasks">{{currentTask.description}}  <button (click)="editTask()">Edit!</button></li>
+        </ul>
      </div>
     `
 })
@@ -20,10 +20,20 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course'),
-    new Task('Begin brainstorming possible JavaScript group projects'),
-    new Task('Add README file to last few Angular repos on GitHub')
-  ];
+  new Task('Finish weekend Angular homework for Epicodus course', 3),
+  new Task('Begin brainstorming possible JavaScript group projects', 2),
+  new Task('Add README file to last few Angular repos on GitHub', 2)
+];
+
+  priorityColor(currentTask){
+   if (currentTask.priority === 3){
+     return "bg-danger";
+   } else if (currentTask.priority === 2) {
+     return  "bg-warning";
+   } else {
+     return "bg-info";
+   }
+  }
 
   editTask() {
     alert("You just requested to edit a Task!");
@@ -40,5 +50,5 @@ export class AppComponent {
 
 export class Task {
   public done: boolean = false;
-  constructor(public description: string) {  }
+  constructor(public description: string, public priority: number) {   }
 }
